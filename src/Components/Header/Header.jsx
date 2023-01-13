b12ab92981365ba2ea65d100d4b53a9410ad8979
Manuperex <manuperex24@gmail.com>
2023-01-13 13:59:02 -0500

    fonts folder, Supertalls font, import to css index.css

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,10 +1,31 @@
-import "./Header.css"
+import './Header.css'
 import React from 'react'
+import LogoToracon from '../../assets/Header/logoToracon.png'
+import Cart from '../../assets/Header/carts.png'
 
 const Header = () => {
   return (
-    <div>
-        <h1>Hola mundo</h1>
+    <div className='Header'>
+        <div className='Header-contents'>
+            <div className='img-logo-Toracon-simple'>
+                <img src={LogoToracon} alt="LogoToracon" />
+            </div>
+            <div>
+                <div>
+                    <h1>
+                        home
+                    </h1>
+                </div>
+                <div>
+                    <h1>
+                        contacto
+                    </h1>
+                </div>
+                <div className='img-cart'>
+                    <img src={Cart} alt="LogoToracon" />
+                </div>
+            </div>
+        </div>
     </div>
   )
 }
