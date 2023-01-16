48201660775a527fee6f1e0881fa868086125ab9
Manuperex <manuperex24@gmail.com>
2023-01-16 12:12:29 -0500

    termino componente carrusel, agrego color negro de fondo a root finish carousel component, add black background color to root

--- src/Components/CardsCarousel/CardsCarousel.jsx
+++ src/Components/CardsCarousel/CardsCarousel.jsx
@@ -11,19 +11,28 @@ import { Pagination } from "swiper";
 import img1 from '../../assets/CardsCaruosel/img1.jpg'
 import img2 from '../../assets/CardsCaruosel/img2.jpg'
 import img3 from '../../assets/CardsCaruosel/img3.jpg'
+import img4 from '../../assets/CardsCaruosel/img4.jpg'
+import img5 from '../../assets/CardsCaruosel/img6.jpg'
+import img6 from '../../assets/CardsCaruosel/img7.jpg'
+import img7 from '../../assets/CardsCaruosel/img8.jpg'
+import img8 from '../../assets/CardsCaruosel/img9.jpg'
+import img9 from '../../assets/CardsCaruosel/img10.jpg'
+import img10 from '../../assets/CardsCaruosel/img11.jpg'
+import img11 from '../../assets/CardsCaruosel/img12.jpg'
+import img12 from '../../assets/CardsCaruosel/img5.jpg'
 const CardsCarousel = () => {
     return (
         <>
           <Swiper 
             slidesPerView={2}
-            spaceBetween={10}
+            spaceBetween={5}
             pagination={{
             clickable: true,
             }}
             breakpoints={{
                 500: {
                   slidesPerView: 3,
-                  spaceBetween: 20,
+                  spaceBetween: 10,
                 },
                 768: {
                   slidesPerView: 4,
@@ -45,12 +54,33 @@ const CardsCarousel = () => {
             <SwiperSlide>
                 <img src={img3} alt="" />
             </SwiperSlide>
-            <SwiperSlide>Slide 4</SwiperSlide>
-            <SwiperSlide>Slide 5</SwiperSlide>
-            <SwiperSlide>Slide 6</SwiperSlide>
-            <SwiperSlide>Slide 7</SwiperSlide>
-            <SwiperSlide>Slide 8</SwiperSlide>
-            <SwiperSlide>Slide 9</SwiperSlide>
+            <SwiperSlide>
+                <img src={img4} alt="" />
+            </SwiperSlide>
+            <SwiperSlide>
+                <img src={img5} alt="" />
+            </SwiperSlide>
+            <SwiperSlide>
+                <img src={img6} alt="" />
+                </SwiperSlide>
+            <SwiperSlide>
+                <img src={img7} alt="" />
+                </SwiperSlide>
+            <SwiperSlide>
+                <img src={img8} alt="" />
+                </SwiperSlide>
+            <SwiperSlide>
+                <img src={img9} alt="" />
+                </SwiperSlide>
+            <SwiperSlide>
+                <img src={img10} alt="" />
+                </SwiperSlide>
+            <SwiperSlide>
+                <img src={img11} alt="" />
+                </SwiperSlide>
+            <SwiperSlide>
+                <img src={img12} alt="" />
+                </SwiperSlide>
           </Swiper>
         </>
     )
