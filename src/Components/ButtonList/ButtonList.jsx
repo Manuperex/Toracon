29c4ceeaf6181e6f321bcb8df14b1b9930a40e07
Manuperex <manuperex24@gmail.com>
2023-01-18 14:44:48 -0500

    Estilo a ButtonList, creacion Entradas

--- src/Components/ButtonList/ButtonList.jsx
+++ src/Components/ButtonList/ButtonList.jsx
@@ -1,19 +1,30 @@
 import React from 'react'
+import "./ButtonList.css"
 
 const ButtonList = ({ categories, filterCategory }) => {
     return (
-		<div className='categories'>
+		<div>
+			<div className='categories-title'>
+				<h1>
+					Entradas
+				</h1>
+			</div>
+			<div className='categories'>
 			{categories.map(category => (
 				<button
 					type='button'
 					className='btn-category'
 					onClick={() => filterCategory(category)}
 					key={category}
-				>
+				><h2>
 					{category}
+				</h2>
 				</button>
 			))}
+			</div>
 		</div>
+
+		
 	);
 }
 
