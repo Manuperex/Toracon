ea7079e1aec71caa5c4ea6a882e19c25218befcf
Manuperex <manuperex24@gmail.com>
2023-01-20 17:08:35 -0500

    Create contact view and its css

--- src/Page/Contact/Contact.jsx
+++ src/Page/Contact/Contact.jsx
@@ -1,8 +1,58 @@
 import React from 'react'
+import "./Contact.css"
+import LogoTitle from "../../assets/Footer/LogoTitle.png";
+import Whatsapp from "../../assets/Footer/whatsapp.png";
+import GitHub from "../../assets/Footer/github.png";
+import Linkedin from "../../assets/Footer/linkedin.png";
+import Instagram from "../../assets/Footer/instagram.png";
+import Facebook from "../../assets/Footer/facebook.png";
 
 const Contact = () => {
   return (
-    <div>Contact</div>
+    <div className="Contact">
+      <div className="Contact-info">
+        <div className="Contact-Logo-title">
+          <img src={LogoTitle} alt="" />
+        </div>
+        <div className="Contact-contact">
+          <div className="Networks-Page">
+            <div>
+              <h3>
+                Contáctanos
+              </h3>
+            </div>
+            <div className="Networks-Contact-img">
+              <div>
+                <img src={Whatsapp} alt="" />
+              </div>
+              <div>
+                <img src={Facebook} alt="" />
+              </div>
+              <div>
+                <img src={Instagram} alt="" />
+              </div>
+            </div>
+            
+          </div>
+          <div className="Developer-Page">
+            <div>
+              <h3>
+                Manuel Pérez
+              </h3>
+            </div>
+            <div className="Developer-Network-img">
+              <div>
+                <img src={GitHub} alt="" />
+              </div>
+              <div>
+                <img src={Linkedin} alt="" />
+              </div>
+            </div>
+            
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
